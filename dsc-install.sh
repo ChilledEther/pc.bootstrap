3b402220d118f9eac5e3eb89950ebd420eeb8b08
ChilledEther <jarredroberts@outlook.com>
2026-01-15 12:35:47 +0000

    refactor: improve DSC installation script with robust error handling, temporary directory management, and verbose output.

--- dsc-install.sh
+++ dsc-install.sh
@@ -1,7 +1,23 @@
-cd /tmp && \
-curl -L "https://github.com/PowerShell/DSC/releases/download/v3.1.2/DSC-3.1.2-x86_64-linux.tar.gz" -o dsc.tar.gz && \
-tar -xzf dsc.tar.gz && \
-sudo mv dsc /usr/local/bin/dsc && \
-sudo chmod +x /usr/local/bin/dsc && \
-rm dsc.tar.gz && \
+#!/bin/bash
+set -e
+
+TMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TMP_DIR"' EXIT
+
+cd "$TMP_DIR"
+
+echo "Downloading DSC v3.1.2..."
+curl -L "https://github.com/PowerShell/DSC/releases/download/v3.1.2/DSC-3.1.2-x86_64-linux.tar.gz" -o dsc.tar.gz
+
+echo "Extracting..."
+tar -xzf dsc.tar.gz
+
+echo "Installing to /usr/local/bin/dsc..."
+if [ -f "/usr/local/bin/dsc" ]; then
+    sudo rm -f /usr/local/bin/dsc
+fi
+sudo mv dsc /usr/local/bin/dsc
+sudo chmod +x /usr/local/bin/dsc
+
+echo "Verifying installation..."
 dsc --version
