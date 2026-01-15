2bdc469055c5cbc36223b2f056245a51601a32c3
ChilledEther <jarredroberts@outlook.com>
2026-01-15 12:32:48 +0000

    chore: streamline DSC installation script into a single command and add version verification.

--- dsc-install.sh
+++ dsc-install.sh
@@ -1,12 +1,7 @@
-# 1. Download the correct x86_64 version
-curl -L "https://github.com/PowerShell/DSC/releases/download/v3.1.2/dsc-3.1.2-x86_64-unknown-linux-gnu.tar.gz" -o dsc.tar.gz
-
-# 2. Extract it
-tar -xzf dsc.tar.gz
-
-# 3. Move it to your path (overwriting the bad one)
-sudo mv dsc /usr/local/bin/dsc
-sudo chmod +x /usr/local/bin/dsc
-
-# 4. Cleanup
-rm dsc.tar.gz
+cd /tmp && \
+curl -L "https://github.com/PowerShell/DSC/releases/download/v3.1.2/dsc-3.1.2-x86_64-unknown-linux-gnu.tar.gz" -o dsc.tar.gz && \
+tar -xzf dsc.tar.gz && \
+sudo mv dsc /usr/local/bin/dsc && \
+sudo chmod +x /usr/local/bin/dsc && \
+rm dsc.tar.gz && \
+dsc --version
