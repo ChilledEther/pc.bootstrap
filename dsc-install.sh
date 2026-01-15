a54d6d236a1efce0a41c9c4fc5041d739e3f0557
ChilledEther <jarredroberts@outlook.com>
2026-01-15 12:33:43 +0000

    fix: update PowerShell DSC package download filename in install script

--- dsc-install.sh
+++ dsc-install.sh
@@ -1,5 +1,5 @@
 cd /tmp && \
-curl -L "https://github.com/PowerShell/DSC/releases/download/v3.1.2/dsc-3.1.2-x86_64-unknown-linux-gnu.tar.gz" -o dsc.tar.gz && \
+curl -L "https://github.com/PowerShell/DSC/releases/download/v3.1.2/DSC-3.1.2-x86_64-linux.tar.gz" -o dsc.tar.gz && \
 tar -xzf dsc.tar.gz && \
 sudo mv dsc /usr/local/bin/dsc && \
 sudo chmod +x /usr/local/bin/dsc && \
